c25156cec4a601700a688fe186ec0bdf60f51c3e
chrispy <christiancarteno@hotmail.com>
2022-10-22 21:52:41 -0700

    a bit of explaining

--- pages/page2.js
+++ pages/page2.js
@@ -3,6 +3,17 @@ export default function page2(){
     return(
         <div>
             <h1>This is Page 2</h1>
+            <h2>
+                Once this page finishes loading this will show up
+            </h2>
+            <p>
+                this is a paragraph within page two,
+                the purpose of this project is to showcase
+                how to use css , html, and javascript in order
+                to show how a responsive website handles
+                loading screens when web pages take a long time to
+                load.
+            </p>
         </div>
     )
 }
